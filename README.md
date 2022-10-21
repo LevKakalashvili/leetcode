180cdf6e3d83cd6fbf1283e5c033f3824aff4360
Lev <kakalashvililev@yandex.ru>
2022-10-21 13:41:18 +0300

    add task 26. Remove Duplicates from Sorted Array

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # leetcode
 ## [1. Two Sum](https://leetcode.com/problems/two-sum/)
+## [26. Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
 ## [169. Majority Element](https://leetcode.com/problems/majority-element/)
 ## [217. Contains Duplicate](https://leetcode.com/problems/contains-duplicate/)
---
+++ tests/test_26_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,14 @@
+from python_.task_26_Remove_Duplicates_from_Sorted_Array import removeDuplicates
+import pytest
+
+
+@pytest.mark.parametrize("nums, expected_result",
+                         [
+                             ([-1,0,0,0,0,3,3], 3),
+                             ([1,1,2], 2),
+                             ([0,0,1,1,1,2,2,3,3,4], 5)
+                         ]
+                         )
+def test_removeDuplicates(nums, expected_result):
+    assert removeDuplicates(nums=nums) == expected_result
+    b = 1
